b4183ef8fc21f95eb593a5dc7f9b573860d36fc6
Nina Drüke <nina.drueke@tapdo.io>
2024-02-20 22:26:06 +0100

    feat: aim in quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -3,11 +3,12 @@ import styles from '@/components/quote.module.scss';
 import { useCallback, useEffect, useRef } from 'react';
 
 interface Props {
+  selectedAim: string;
   title: string;
   quotes: string[];
   onClose: () => void;
 }
-export default function Quote({ title, quotes, onClose }: Props) {
+export default function Quote({ selectedAim, title, quotes, onClose }: Props) {
   const sanitizedTitle = title.replaceAll('-\n', '');
 
   const backgroundRef = useRef<HTMLDivElement | null>(null);
@@ -34,9 +35,14 @@ export default function Quote({ title, quotes, onClose }: Props) {
     <div className={styles['gwb-quote']} ref={backgroundRef}>
       <div className={styles['gwb-quote__popup']} ref={popupRef}>
         <div className={styles['gwb-quote__popup__header']}>
-          <h3 className={styles['gwb-quote__popup__header__title']}>
-            {sanitizedTitle}
-          </h3>
+          <div className={styles['gwb-quote__popup__header__title']}>
+            <h5 className={styles['gwb-quote__popup__header__title__overline']}>
+              {selectedAim}
+            </h5>
+            <h3 className={styles['gwb-quote__popup__header__title__title']}>
+              {sanitizedTitle}
+            </h3>
+          </div>
           <button
             className={styles['gwb-quote__popup__header__button']}
             onClick={onClose}
